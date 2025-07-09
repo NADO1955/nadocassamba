3f5afc100907f388056b3cecc6e5ac2f4804fbfb
NADO CASSAMBA <bernardocassamba@gmail.com>
2025-07-09 02:22:50 +0100

    Renomeado RCU para rcu para compatibilidade com ambiente Linux

--- app/Models/Rcu.php
+++ app/Models/Rcu.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class RCU extends Model
+class rcu extends Model
 {
     use HasFactory;
 
